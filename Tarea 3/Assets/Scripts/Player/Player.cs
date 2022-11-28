a17787a1be202e411e203fb07b6e44b2fd94221a
RedHood02 <103451619+RedHood02@users.noreply.github.com>
2022-11-28 18:00:58 -0300

    pene

--- Tarea 3/Assets/Scripts/Player/Player.cs
+++ Tarea 3/Assets/Scripts/Player/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 
     [SerializeField] float moveSpeed;
 
+    [SerializeField] Animator playerAnim;
+
 
     // Start is called before the first frame update
     void Start()
---
+++ Tarea 3/Assets/Scripts/Story/First Room/NightStand3.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class NightStand3 : StoryObjectInteraction, IInteracteable
+{
+    /*[SerializeField]*/ TMP_Text text;
+    private void Update()
+    {
+        Interact();
+    }
+
+    public void Interact()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && withinRange)
+        {
+            dialogueBoxAnim.Play("TextBoxSpawn");
+            BoxTextChanger();
+            nightstandAnim.SetBool("withinRange", false);
+        }
+
+        if (!withinRange)
+        {
+            dialogueBoxAnim.Play("TextBoxDespawn");
+            nightstandAnim.SetBool("withinRange", false);
+        }
+    }
+
+    void BoxTextChanger()
+    {
+        text = (TMP_Text)FindObjectOfType(typeof(TMP_Text));
+        text.text = "Nighstand 3";
+    }
+
+}
